cef7c2ca26b1cf1f0f141911546c0f06968dd75a
Zaneeyboy <40801075+Zaneeyboy@users.noreply.github.com>
2020-10-20 15:05:06 -0400

    CSS Media Queries

--- js/index.js
+++ js/index.js
@@ -1,27 +1,13 @@
+const navOpener=document.getElementById("navbar-opener");
+const navCloser=document.getElementById("modal-closer");
+const modal=document.getElementById("menu-modal");
 
-let flag = (window.innerWidth < 400) ? true : false;
+navOpener.addEventListener("click",()=>{
+    console.log("Modal Opened");
+    modal.style.display="block";
+});
 
-
-if(flag){
-    const burger=document.getElementById("burger");
-    const x=document.getElementById("x");
-    const body=document.getElementById("body");
-
-    burger.addEventListener("click",()=>{
-        burger.style.display="none";
-        x.style.display="inline-block";
-        body.classList.add("opaque");
-        
-
-        x.addEventListener("click",()=>{
-            burger.style.display="inline-block";
-            x.style.display="none";
-            body.classList.remove("opaque");
-        });
-    });
-}
-else{
-    console.log("Screen too big");
-}
-
-
+navCloser.addEventListener("click",()=>{
+    console.log("Modal Closed")
+    modal.style.display="none";
+});
